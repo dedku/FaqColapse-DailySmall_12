ce279825baa30affff5910c50ac169a867a10639
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-20 13:47:16 +0100

    ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './style.css'
+
+const toggles = document.querySelectorAll('.faq-toggle') as NodeListOf<HTMLButtonElement>
+
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    // @ts-ignore
+    toggle.parentNode!.classList.toggle('active')
+  })
+})
